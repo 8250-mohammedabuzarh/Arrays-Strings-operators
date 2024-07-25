1999759788267aa3acd15dc8deec13795821c54d
Mohammed Abuzarh <Mohammed.Abuzarh@godigit.com>
2024-07-25 17:40:59 +0530

    Divisability check program

---
+++ Operators/Divisability.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class Divisability {
+    public static void main(String[] args)
+    {
+        int n=35;
+        if(n%5==0 && n%7==0)
+        {
+            System.out.println("Divisible by 5 and 7");
+        }
+        else{
+            System.out.println("Not Divisible by 5 and 7");
+        }
+    }
+}
