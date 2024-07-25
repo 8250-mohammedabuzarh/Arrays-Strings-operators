dda2f4575bdc8fb64923fe84c0ed496a2d148f8e
Mohammed Abuzarh <Mohammed.Abuzarh@godigit.com>
2024-07-25 17:41:37 +0530

    Increment operators program

---
+++ Operators/Increments.java
@@ -0,0 +1,19 @@
+package Operators;
+
+public class Increments {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b= a++;
+        System.out.println(b);
+        int c=10;
+        int d= c--;
+        System.out.println(d);
+        int e=10;
+        int f= ++e;
+        System.out.println(f);
+        int g=10;
+        int h= --g;
+        System.out.println(h);
+    }
+}
