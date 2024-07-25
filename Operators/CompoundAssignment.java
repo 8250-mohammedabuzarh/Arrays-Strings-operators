6d0aef023125d3854ef9fa90dda23e61624c3bfd
Mohammed Abuzarh <Mohammed.Abuzarh@godigit.com>
2024-07-25 17:40:25 +0530

    compound assignment operators program

---
+++ Operators/CompoundAssignment.java
@@ -0,0 +1,24 @@
+package Operators;
+
+public class CompoundAssignment {
+    public static void main(String[] args)
+    {
+        int a=10;
+       int b= a+=5;
+        System.out.println(b);
+        int c=10;
+        int d= c-=5;
+        System.out.println(d);
+        int e=10;
+        int f= e*=5;
+        System.out.println(f);
+        int g=10;
+        int h= g/=5;
+        System.out.println(h);
+        int i=10;
+        int j= i%=5;
+        System.out.println(j);
+
+
+    }
+}
