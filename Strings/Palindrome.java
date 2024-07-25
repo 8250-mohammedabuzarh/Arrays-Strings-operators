179065a43dd9f9ccfb9c8a50254ee7ec40163784
Mohammed Abuzarh <Mohammed.Abuzarh@godigit.com>
2024-07-25 17:49:11 +0530

    program to check Palindrome String

---
+++ Strings/Palindrome.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class Palindrome {
+    public static void main(String[] args){
+        String str = "ALDCAACDLA";
+        String rev = "";
+        for(int i = str.length()-1; i >= 0; i--){
+            rev+=str.charAt(i);
+        }
+        if(str.equals(rev))
+        {
+            System.out.println("Palindorme ");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+
+    }
+}
