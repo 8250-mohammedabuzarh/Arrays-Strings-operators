2b73c1b2f551d679790152256f0dd39f085915f9
Mohammed Abuzarh <Mohammed.Abuzarh@godigit.com>
2024-07-25 17:44:11 +0530

    Toggle Boolean program

---
+++ Operators/ToggleBoolean.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class ToggleBoolean {
+    public static void main(String[] args)
+    {
+        boolean a=true;
+        boolean b=!a;
+        System.out.println(b);
+    }
+}
